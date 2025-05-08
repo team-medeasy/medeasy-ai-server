fb4eefbc0672c032a163869c0ce11f054f94df60
jiwon <angry9908@gmail.com>
2025-05-08 18:27:19 +0900

    feat: mcp client

--- mcp_client/client.py
+++ mcp_client/client.py
@@ -8,29 +8,16 @@ import logging
 load_dotenv()
 logger = logging.getLogger(__name__)
 
-# MCP 클라이언트 초기화
-config = {
-      "mcpServers": {
-        "fastapi-mcp": {
-          "command": "npx",
-          "args": [
-            "mcp-remote",
-            "http://medeasy-mcp-server-service:30003/mcp",
-            "8080",
-            "--allow-http",
-          ]
-      }
-  }
-}
+config_path = os.getenv("MCP_CONFIG_PATH", "mcp_client_config/medeasy_mcp_client.json")
+
 # Create LLM
 llm = ChatOpenAI(model_name="gpt-4o-mini")
 
 # Create MCPClient with config
-mcp_client = MCPClient.from_dict(config)
+mcp_client = MCPClient.from_config_file(config_path)
 
 # Create agent with the client
-agent = MCPAgent(llm=llm, client=mcp_client, max_steps=30)
-
+agent = MCPAgent(llm=llm, client=mcp_client, use_server_manager=True, max_steps=20)
 
 async def process_user_message(user_message):
 
