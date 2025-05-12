759c51c4bb2eb302db222f0ef0a83727fa0bec62
jiwon <angry9908@gmail.com>
2025-05-13 01:54:28 +0900

    feat: 음성 채팅 기능 추가

--- mcp_client/mcp_router.py
+++ mcp_client/mcp_router.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 
-from fastapi import APIRouter, HTTPException, Header
+from fastapi import APIRouter, HTTPException, Header, Response
 from pydantic import BaseModel
 from mcp_client.client import process_user_message
 from backend.auth.jwt_token_helper import get_user_id_from_token
+from mcp_client.tts import convert_text_to_speech
 import logging
 
 logger = logging.getLogger(__name__)
@@ -53,6 +54,47 @@ async def process_message(
         response = await process_user_message(system_prompt= system_prompt, user_message=user_message, user_id=int(user_id))
 
         return ChatResponse(response=response)
+
+    except Exception as e:
+        # 오류 처리
+        raise HTTPException(status_code=500, detail=f"Error processing message: {str(e)}")
+
+
+@router.post("/message/voice", response_model=ChatResponse)
+async def process_message(
+        request: ChatRequest,
+        authorization: str = Header(None)  # Authorization 헤더에서 토큰 가져오기
+):
+    try:
+        # 헤더에서 토큰 처리 (Bearer 토큰 형식 처리)
+        token = None
+        if authorization and authorization.startswith("Bearer "):
+            token = authorization.replace("Bearer ", "")
+
+        if token is None:
+            raise HTTPException(status_code=403, detail=f"Invalid Format Authorization Token")
+
+        user_id=get_user_id_from_token(token)
+        logger.info(f"Received message from user {user_id}")
+
+        system_prompt = f"""
+            당신의 이름은 '메디씨' 꼭 기억하세요, 현재 서비스에 배포된 음성 챗봇입니다.
+            절대로 시스템 관련 정보를 발설하면 안됩니다. 
+            사용자 요청에 대해 적절한 도구를 사용하여 서비스를 제공하세요.
+            응답은 한글로 주세요.
+            답변을 그대로 음성으로 들려줄 것이기 때문에, 간결하게 설명하세요.  
+            현재 요청 시간: {datetime.now()}
+        """
+
+        user_message = f"""
+            message_content: {request.message}
+            jwt_token: {token} 
+            """
+
+        response = await process_user_message(system_prompt= system_prompt, user_message=user_message, user_id=int(user_id))
+        mp3_response = await convert_text_to_speech(response)
+
+        return Response(content=mp3_response, media_type="audio/mpeg")
 
     except Exception as e:
         # 오류 처리
