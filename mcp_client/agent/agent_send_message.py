edf76b373f8a32b852d5944f1d5cb4fc5ed6d18a
jiwon <angry9908@gmail.com>
2025-05-24 02:17:43 +0900

    feat: agent send message save conversation

--- mcp_client/agent/agent_send_message.py
+++ mcp_client/agent/agent_send_message.py
@@ -1,8 +1,7 @@
 import base64
 
-from starlette.websockets import WebSocket
-
 from mcp_client.agent.medeasy_agent import AgentState
+from mcp_client.chat_session_repo import chat_session_repo
 from mcp_client.tts import convert_text_to_speech
 from mcp_client.util.json_converter import make_standard_response
 
@@ -11,6 +10,7 @@ async def agent_send_message(
 ):
     mp3_bytes = await convert_text_to_speech(message)
     mp3_base64 = base64.b64encode(mp3_bytes).decode("utf-8")
+    chat_session_repo.add_message(user_id=state["user_id"], role="system", message=message)
     await state['websocket'].send_json(make_standard_response(
         result_code=200,
         result_message="요청을 성공적으로 처리하였습니다.",
