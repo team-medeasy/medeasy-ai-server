0285669d0acb0ebca8b032f45f985300add09502
jiwon <angry9908@gmail.com>
2025-05-24 02:11:36 +0900

    refactor: router method rename

--- mcp_client/agent/node/__init__.py
+++ mcp_client/agent/node/__init__.py
@@ -3,7 +3,7 @@ from mcp_client.agent.node.check_server_actions import check_server_actions
 
 from mcp_client.agent.node.load_tools import load_tools, has_error
 from mcp_client.agent.node.generate_initial_response import generate_initial_response
-from mcp_client.agent.node.check_client_actions import check_client_actions, has_capture_request
+from mcp_client.agent.node.check_client_actions import check_client_actions
 from mcp_client.agent.node.execute_tools import execute_tools, has_tool_calls
 from mcp_client.agent.node.generate_final_response import generate_final_response
 from mcp_client.agent.node.detect_conversation_shift import detect_conversation_shift, direction_router
@@ -27,5 +27,4 @@ __all__ = [
     "has_error",
     "has_server_action",
     "has_tool_calls",
-    "has_capture_request",
 ]
