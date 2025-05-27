2d3636e02b29c10bfaf79f77f75cd7e82444f534
jiwon <angry9908@gmail.com>
2025-05-28 04:05:24 +0900

    refactor: nave tts로 변경

--- mcp_client/agent/agent_send_message.py
+++ mcp_client/agent/agent_send_message.py
@@ -2,13 +2,13 @@ import base64
 
 from mcp_client.agent.medeasy_agent import AgentState
 from mcp_client.chat_session_repo import chat_session_repo
-from mcp_client.tts import convert_text_to_speech
+from mcp_client.tts.clova_tts import convert_text_to_speech
 from mcp_client.util.json_converter import make_standard_response
 
 async def agent_send_message(
         state: AgentState, message: str
 ):
-    mp3_bytes = await convert_text_to_speech(message)
+    mp3_bytes = await convert_text_to_speech(user_id=state["user_id"], text=message)
     mp3_base64 = base64.b64encode(mp3_bytes).decode("utf-8")
     chat_session_repo.add_message(user_id=state["user_id"], role="system", message=message)
     await state['websocket'].send_json(make_standard_response(
